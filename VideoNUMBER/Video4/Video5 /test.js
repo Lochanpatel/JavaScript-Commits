ac9047bd43e191daa5b18c404891a4a55f8f0af6
Lochanpatel <112246458+Lochanpatel@users.noreply.github.com>
2025-03-12 20:22:49 +0000

    commit

---
+++ VideoNUMBER/Video4/Video5 /test.js
@@ -0,0 +1,15 @@
+"use strict"; // treat all js code as newer version
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+let age = 18
+// object
+console.log(typeof age);
+console.log(typeof null);        //object
+console.log(typeof undefined);   //undefined
