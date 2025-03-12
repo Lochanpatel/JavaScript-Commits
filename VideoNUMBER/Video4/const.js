bd4e2b29cf01c96cbc9e8dd44afc1c87a06af44c
Lochanpatel <112246458+Lochanpatel@users.noreply.github.com>
2025-03-12 20:01:40 +0000

    push

---
+++ VideoNUMBER/Video4/const.js
@@ -0,0 +1,9 @@
+const acountId = 3333
+let accountEmail = "lochan@goole.com"
+var accountPassword = "33333333"  //dont use var because of block scope and functional scope 
+accountCity = "indore"
+
+// acountId=3  not allowed
+console.log(acountId);
+console.table([accountEmail,accountPassword])
+
