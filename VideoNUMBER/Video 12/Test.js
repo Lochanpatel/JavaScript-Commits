8cbedf023b60d7b4d761190ffee52a355914c510
Lochanpatel <112246458+Lochanpatel@users.noreply.github.com>
2025-03-17 18:59:54 +0000

    commit

---
+++ VideoNUMBER/Video 12/Test.js
@@ -0,0 +1,32 @@
+const score = 100
+// console.log(score)
+
+const balance = new Number(400)
+// console.log(balance);
+// console.log(balance.toString());
+
+const value = 10000000
+// console.log(value.toLocaleString('en-IN'));
+
+console.log(Math);
+console.log(Math.abs(-54));
+
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4, 3, 6, 8));
+// console.log(Math.max(4, 3, 6, 8));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+
---
+++ VideoNUMBER/Video 14/Array.js
@@ -0,0 +1,33 @@
+const myArr = [0, 1, 2, 3, 4];
+const myarray = ["lochan", "patel", "patidar"];
+
+const myarrayn = new Array(1, 2, 3, 4);
+
+// console.log(myArr[2]);
+
+// console.log(myarray[1]);
+// console.log(myarrayn)
+
+// myarr.push(3)
+// myarr.pop()
+
+// console.log(myarr.includes(3));
+
+
+
+
+
+
+// slice, splice
+
+console.log("A ", myArr);
+
+const myn1 = myArr.slice(1, 3)
+
+console.log(myn1);
+console.log("B ", myArr);
+
+
+const myn2 = myArr.splice(1, 3)
+console.log("C ", myArr);
+console.log(myn2);
