3cde22cfe48c5ffe7f86c2eab4ea6700f7effec6
Lochanpatel <112246458+Lochanpatel@users.noreply.github.com>
2025-03-17 20:53:01 +0000

    Day 4

---
+++ VideoNUMBER/Video 16/Test.js
@@ -0,0 +1,16 @@
+// singelton method 
+// Object.create
+
+// object literals
+const myobj = {
+    name : 'lochan',
+    age : 33,
+    location: 'indore',
+    email: "patel@gmail.com"
+
+}
+console.log(myobj);
+console.log(myobj.location);
+
+
+
